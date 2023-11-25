51856d4a1b9e5db8ae2a059233ef8e000d48d74d
Juan Motta <juanandres.lopez@crehana.com>
2023-11-25 18:42:23 -0500

    feat: added microsoft clarity service

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,28 +2,13 @@ import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
 import { useEffect } from 'react';
+import { clarity } from 'react-microsoft-clarity';
 
 import CustomThemeProvider from '@/providers/CustomThemeProvider';
 
 export default function App({ Component, pageProps }: AppProps) {
-    // esint-disable-next-line
-    // @ts-ignore
-    function clarityInit(c, l, a, r, i, t, y) {
-        c[a] =
-            c[a] ||
-            function () {
-                (c[a].q = c[a].q || []).push(arguments);
-            };
-        t = l.createElement(r);
-        t.async = 1;
-        t.src = 'https://www.clarity.ms/tag/' + i;
-        y = l.getElementsByTagName(r)[0];
-        y.parentNode.insertBefore(t, y);
-    }
     useEffect(() => {
-        // eslint-disable-next-line
-        // @ts-ignore
-        clarityInit(window, document, 'clarity', 'script', 'jvtkzru24o');
+        clarity.init('jvtkzru24o');
     }, []);
     return (
         <CustomThemeProvider>
